8e09c68b6f79add5f0c9a1102dab68a480d27500
Eldad Uzman <eldadu1985@gmail.com>
2022-09-24 12:15:50 +0300

    create a realistic test case

--- utests/test_test_plan.py
+++ utests/test_test_plan.py
@@ -1,14 +1,19 @@
 """unittest module"""
-from unittest import TestCase, mock, main
+from unittest import TestCase, main
+from pymeter.api.config import TestPlan
 
 
-class TestInstanceCreation(TestCase):
-    """This suite tests the creation of new gevent instances"""
+class TestTestPlanClass(TestCase):
+    """"""
 
-    def test_instantiate_keywords_class(self):
-        """when a new keyword instance is created,
-        it should have no bundle of coroutines"""
-        self.assertEqual(0, 0)
+    def test_creation_of_empty_test_plan(self):
+        """when creating the python class, it should wrap around the correct java class"""
+        python_test_plan_object = TestPlan()
+        java_test_plan_object = python_test_plan_object.java_wrapped_element
+        self.assertEqual(
+            str(type(java_test_plan_object)),
+            "<class 'jnius.reflect.us.abstracta.jmeter.javadsl.core.DslTestPlan'>",
+        )
 
 
 if __name__ == "__main__":
