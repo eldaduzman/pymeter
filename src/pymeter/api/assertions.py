17fc1d19713d39ef7d1669d68f2a2bd7a009d77a
Eldad Uzman <eldadu1985@gmail.com>
2022-11-19 19:41:17 +0200

    [skip ci] fix badges

--- src/pymeter/api/assertions.py
+++ src/pymeter/api/assertions.py
@@ -1,10 +1,13 @@
 """
 By default, JMeter marks any HTTP request with a fail response code (4xx or 5xx) as failed,
-which allows you to easily identify when some request unexpectedly fails. 
+which allows you to easily identify when some request unexpectedly fails.
 
-But in many cases, this is not enough or desirable, and you need to check for the response body (or some other field) to contain (or not) a certain string.
+But in many cases, this is not enough or desirable,
+and you need to check for the response body (or some other field) to contain (or not) a certain string.
 
-This is usually accomplished in JMeter with the usage of Response Assertions, which provides an easy and fast way to verify that you get the proper response for each step of the test plan, marking the request as a failure when the specified condition is not met.
+This is usually accomplished in JMeter with the usage of Response Assertions,
+which provides an easy and fast way to verify that you get the proper response for each step of the test plan,
+marking the request as a failure when the specified condition is not met.
 
 example - 1:
 --------------
@@ -12,6 +15,7 @@ In this example we assert that the response contains the text "var"
 
 
       .. code-block:: python
+
             from pymeter.api.config import TestPlan, ThreadGroupSimple
             from pymeter.api.samplers import HttpSampler
             from pymeter.api.timers import ConstantTimer
